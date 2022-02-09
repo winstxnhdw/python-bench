79d3e2ecccac8dc430e7af524e626419ca893479
winstxnhdw <winstxnhdw@gmail.com>
2022-02-10 02:11:10 +0800

    update

--- src/libs/print_bench.py
+++ src/libs/print_bench.py
@@ -1,4 +1,6 @@
+from string import capwords
+
 def print_bench(names, outputs):
 
     for name, output in zip(names, outputs):
-        print(f"\n{name.capitalize()} test:\n{output}")
+        print(f"\n{capwords(name)} test:\n{output}")
